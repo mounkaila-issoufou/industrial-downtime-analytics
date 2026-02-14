c613b38d2c84744c3b95ae6da345fc346e30b7e1
Mounkaila Issoufou <abdoulrazak034@gmail.com>
2026-02-14 12:10:48 +0100

    chore(pipeline): fix execution order and align with updated staging and dw schemas

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from src.utils.logger import logger
 from src.config.config import RAW_DATA_DIR
-
+from src.ingestion.run_generation import run_data_generation_pipeline
 
 # =========================
 # Database pipeline
@@ -20,11 +20,22 @@ def main():
     # =========================
     logger.info("▶ Starting Python data pipeline")
 
+    # =========================
+    # 1. Generate mock production data
+    # =========================
+    logger.info("▶ Generating mock production data")
+    run_data_generation_pipeline()
+    logger.info("✔ Mock data generated")
 
     # =========================
     # 2. DATABASE STRUCTURE
     # =========================
     logger.info("▶ Initializing database schemas & tables")
+    run_sql_file(SQL_DIR / "ddl/00_create_staging_schema.sql")
+    run_sql_file(SQL_DIR / "ddl/19_create_stg_shift_supervision_table.sql")
+    run_sql_file(SQL_DIR / "ddl/20_create_stg_shift_operator_assignment_table.sql")
+    run_sql_file(SQL_DIR / "ddl/21_create_stg_hourly_production_table.sql")
+    run_sql_file(SQL_DIR / "ddl/22_create_stg_production_events_table.sql")
 
     run_sql_file(SQL_DIR / "ddl/01_create_operational_schema.sql")
     run_sql_file(SQL_DIR / "ddl/02_create_factory_table.sql")
@@ -61,20 +72,41 @@ def main():
             )
 
     run_sql_file(
+                SQL_DIR / "dml/03_load_stg_hourly_production.sql",
+                RAW_DATA_DIR / "hourly_production.csv"
+            )
+
+    run_sql_file(
                 SQL_DIR / "dml/04_load_stg_production_events.sql",
                 RAW_DATA_DIR / "production_events.csv"
             )
 
-    run_sql_file(
-                SQL_DIR / "dml/03_load_stg_hourly_production.sql",
-                RAW_DATA_DIR / "hourly_production.csv"
-            )
     logger.info("✔ Staging loaded")
 
     # =========================
     # 4. DATA WAREHOUSE LOAD
     # =========================
     logger.info("▶ Reseting dimensions & facts")
+    run_sql_file(SQL_DIR / "dml/05_load_ops_factory.sql")
+    run_sql_file(SQL_DIR / "dml/06_load_ops_operator.sql")
+    run_sql_file(SQL_DIR / "dml/07_load_ops_team_lead.sql")
+    run_sql_file(SQL_DIR / "dml/08_load_ops_workshop.sql")
+    run_sql_file(SQL_DIR / "dml/09_load_ops_production_line.sql")
+    run_sql_file(SQL_DIR / "dml/10_load_ops_shift_supervision.sql")
+    run_sql_file(SQL_DIR / "dml/11_load_ops_shift_operator_assignment.sql")
+    run_sql_file(SQL_DIR / "dml/12_load_ops_hourly_production.sql")
+    run_sql_file(SQL_DIR / "dml/13_load_ops_production_events.sql")
+
+    run_sql_file(SQL_DIR / "dml/14_load_dim_machine.sql")
+    run_sql_file(SQL_DIR / "dml/15_load_dim_time.sql")
+
+    run_sql_file(SQL_DIR / "dml/16_load_dim_team.sql")
+    run_sql_file(SQL_DIR / "dml/17_load_dim_organe_element.sql")
+    
+    run_sql_file(SQL_DIR / "dml/18_load_fact_hourly_performance.sql")
+    run_sql_file(SQL_DIR / "dml/19_load_fact_production_events.sql")
+
+
 
 
 
